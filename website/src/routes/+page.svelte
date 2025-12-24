27ef2ae32113df7fde88945c2c299a6a96705c67
thereal-atom <oscarfal2006@gmail.com>
2025-12-24 13:43:32 +0000

    feat: fun things

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,3 +1,13 @@
 <div class="w-screen h-screen bg-black flex items-center justify-center">
-    <p class="text-orange-500">[object Object]</p>
+	<p class="text-[#FF4F00] text-2xl">[object Object]</p>
 </div>
+<div class="absolute left-1/2 -translate-x-1/2 bottom-12">
+	<p class="text-sm text-white">
+		made with {'🫶'} by <a href="https://oscar.falemara.com" class="text-[#635BFF]">oscar</a>
+	</p>
+</div>
+<img
+	class="absolute left-8 top-8 w-8 h-8 rounded-sm"
+	src="/oo-icon.svg"
+	alt="[object Object].dev"
+/>
